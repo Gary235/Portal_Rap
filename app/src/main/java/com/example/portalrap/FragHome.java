e39888a9100ce19b1011d7f8a253ce893bb321c1
Gary <berkman.gary@gmail.com>
2020-07-19 21:57:56 -0300

    ''

--- app/src/main/java/com/example/portalrap/FragHome.java
+++ app/src/main/java/com/example/portalrap/FragHome.java
@@ -7,6 +7,8 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -30,18 +32,37 @@ import java.util.Random;
 
 public class FragHome extends Fragment implements View.OnClickListener {
 
-    Button btnEntrenar;
+    //Button btnEntrenar;
+    ImageButton btnEntrenar;
+    ImageView fondo;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
-        View v = inflater.inflate(R.layout.home, container, false);
+        View v = inflater.inflate(R.layout.home_2, container, false);
 
+
+        fondo = v.findViewById(R.id.fondo);
         btnEntrenar = v.findViewById(R.id.btnentrenar);
         btnEntrenar.setOnClickListener(this);
 
         //if (!MainActivity.TodosPermisos) btnEntrenar.setEnabled(false);
 
-            return v;
+        int aleatorio = (int) (Math.random() * 3) ;
+
+        switch (aleatorio){
+            case 0:
+                fondo.setImageResource(R.drawable.ic_fondo_home_1);
+                break;
+            case 1:
+                fondo.setImageResource(R.drawable.ic_fondo_home_2);
+                break;
+            case 2:
+                fondo.setImageResource(R.drawable.ic_fondo_home_3);
+                break;
+
+        }
+        return v;
     }
 
     @Override
