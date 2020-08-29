d27b6a4434023c69e443dadf34cf7a81a029cae1
Gary <berkman.gary@gmail.com>
2020-08-29 03:50:01 -0300

    ..

--- app/src/main/java/com/example/portalrap/FragmentsInicio/FragFoto3.java
+++ app/src/main/java/com/example/portalrap/FragmentsInicio/FragFoto3.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageButton;
 
 import androidx.annotation.Nullable;
@@ -15,7 +16,7 @@ import com.example.portalrap.R;
 
 public class FragFoto3 extends Fragment implements View.OnClickListener {
 
-    ImageButton btn;
+    Button btn;
 
     @Nullable
     @Override
